fe1aec96fc1dfa385b5e7114875c2d59f925335c
Juann2408 <juayalaar@unal.edu.co>
2025-06-11 22:47:25 -0500

    PRUEBA 1 DE MOVIMIENTO (NO COMPLETA)

--- Inicio.py
+++ Inicio.py
@@ -1,37 +1,55 @@
 
 #Bienvenida
-
-
 import random
 Dado1 = random.randint(1, 6)
 Dado2=random.randint(1, 6)
-print("El resultado es {} y {}".format(Dado1,Dado2))
-Mueve1= (Dado1 + Dado2)
-comomoverse=int(input("¿Desea mover únicamente una ficha o dos? Una: 1, Dos: 2 "))
+Tablero= [()]*20
+print(Tablero)
+def movimiento1(Ficha1, Ficha2):
+  while Tablero[Ficha1] != 20 :
+    Dado1 = random.randint(1, 6)
+    Dado2=random.randint(1, 6)
+    print("El resultado es {} y {}".format(Dado1,Dado2))
+    Mueve1= (Dado1 + Dado2)
+    comomoverse=int(input("¿JUGADOR 1. Desea mover únicamente una ficha o dos? Una: 1, Dos: 2 "))
+    if comomoverse==1:
+      Ficha1+=Mueve1
+      print(Ficha1)
+    elif comomoverse==2:
+      CUALFICHA1=int(input("¿JUGADOR 1. ¿Qué ficha desea mover {} movimientos? (1 o 2)".format(Dado1)))
+      CUALFICHA2=int(input("¿JUGADOR 1. ¿Qué ficha desea mover {} movimientos? (1 o 2)".format(Dado2)))
+      if CUALFICHA1==1:
+        Ficha1+=Dado1
+        Ficha2+=Dado2
+        print(Ficha1)
+        print(Ficha2)
 
-if comomoverse==1:
-  Ficha=int(input("¿Que ficha desea mover? (Indique el número de ficha) "))
-  if Ficha == 1:
-    print("F1 se ha movido {} espacios".format(Mueve1))
-  elif Ficha == 2:
-      print("F2 se ha movido {} espacios".format(Mueve1))
-  elif Ficha == 3:
-        print("F3 se ha movido {} espacios".format(Mueve1))
-  elif Ficha == 4:
-          print("F4 se ha movido {} espacios".format(Mueve1))
-  elif Ficha == 2:
-   print("F1 se ha movido {} espacios".format(Mueve1))
+      elif CUALFICHA2==2:
+        Ficha2+=Dado2
+        Ficha1+=Dado1
+      print(Ficha2)
+      print(Ficha1)
+      if Ficha1 or Ficha2 >= len(Tablero):
+        break
+  return(Ficha1, Ficha2)
+  
 
-elif comomoverse==2:
-  F1=int(input("¿Que ficha desea mover? "))
-  F2=int(input("¿Que ficha desea mover? "))
+def movimiento2(Ficha2):
+    while Tablero[Ficha2] != 20 :
+      Dado1 = random.randint(1, 6)
+      Dado2=random.randint(1, 6)
+      print("El resultado es {} y {}".format(Dado1,Dado2))
+      Mueve2= (Dado1 + Dado2)
+      comomoverse=int(input("¿JUGADOR 2. Desea mover únicamente una ficha o dos? Una: 1, Dos: 2 "))
+      if comomoverse==1:
+        Ficha2+=Mueve2
+        print(Ficha2)
+        if Ficha2 >= len(Tablero):
+          break
+    return(Ficha2)
+Ficha1=0
+Ficha2=0
 
-Lados= [n  for n in range (9)]
-for l in (Lados):
-  l=["▭"]*9
-  for i in l:
-    i=["▭"]
-    for h in i:
-      print(h, end="  ")
-  print()
+Ficha1, Ficha2 = movimiento1(Ficha1, Ficha2), movimiento2(Ficha2)
+
 
