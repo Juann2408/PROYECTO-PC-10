b23af23d8a750357575501591bcc598a17f9f058
Juann2408 <juayalaar@unal.edu.co>
2025-07-14 10:12:33 -0500

    APROBADO

--- MOV 1.py
+++ MOV 1.py
@@ -8,26 +8,6 @@ fichas={
 "Ficha 1R":{"posicion":0, "estado": "activa"},
 "Ficha 2AZ":{"posicion":0, "estado": "activa"},
 }  
-def TURNOS (ficha_nombre):
-    dado=random.randint(1,6)
-    print(f"{ficha_nombre} sacó {dado}")
-    input("ENTER para continuar")
-    SacarFicha()
-
-def INICIO ():
-    while True:
-        if fichas["Ficha 1R"]["posicion"]==68:
-            break
-        else:
-            TURNOS("Ficha 1R")
-
-        if fichas["Ficha 2AZ"]["posicion"]==68:
-            break
-        else:
-            TURNOS("Ficha 2AZ" )
-        
-          
-
 
 def actuTab():
     global Casillas
@@ -39,51 +19,64 @@ def actuTab():
 
     print(" ".join(Casillas)) 
 
-def DadosGen():
+
+def moverfichas(ficha_nombre, Cant):
+    ficha = fichas[ficha_nombre]
+    ficha["posicion"]+= Cant
+    actuTab()
+
+def DadosGen(ficha_nombre):
     DadoUno = random.randint(1,6)
     DadoDos= random.randint(1,6)
     SumaAmbos= DadoUno + DadoDos
     print(f"Sacó en 1: {DadoUno}, y en 2: {DadoDos} eso suma un total de...: {SumaAmbos}")
     input("ENTER para acepatar")
-def moverfichas(ficha_nombre, cant):
+
+    moverfichas(ficha_nombre, SumaAmbos)
+
+
+def SacarFicha(ficha_nombre,dado):
     ficha = fichas[ficha_nombre]
-    ficha["posicion"]+=cant
+    if ficha["posicion"]==0:
+        if dado == 5:
+            ficha["posicion"]= 1
+            print("Ficha 1R ha entrado al juego")
+            actuTab()
+        else:
+            print("Eso no parece ser un 5... TURNO DE: FICHA 2AZ  ")
+            
+    else: 
+        print("Esta ficha ya está afuera")
+        DadosGen(ficha_nombre)
+            
+def TURNOS (ficha_nombre):
+    dado = random.randint(1,6)
+    print(f"{ficha_nombre} sacó {dado}")
+    input("ENTER para continuar")
+    SacarFicha(ficha_nombre,dado)
+
+def INICIO ():
+    banana = True
+    while banana == True:
+        TURNOS("Ficha 1R")
+        if fichas["Ficha 1R"]["posicion"]==68:
+            banana=False
+
+            break
+
+        TURNOS("Ficha 2AZ" )
+
+        if fichas["Ficha 2AZ"]["posicion"]==68:
+            break
+
+INICIO()
+        
+          
+
     
 
 
 
-
-def dadoA():
-
-    Dado1 = random.randint(1, 6)
-    print("JUGADOR 1, Sacó",Dado1)
-    input("Presione enter para seguir")
-    SacarFicha(Dado1)
-
-def dadoB():
-
-    Dado1 = random.randint(1, 6)
-    print("JUGADOR 2, Sacó",Dado1)
-    input("Presione enter para seguir")
-    SacarFicha2(Dado1)
-
-
-def SacarFicha(ficha_nombre, dado):
-    ficha = fichas["ficha_nombre"]
-    if ficha["posicion"]==0:
-        ficha = fichas["Ficha 1R"]  
-        if dado == 5 and ficha["posicion"]==0:
-            ficha["posicion"]=1
-            print("Ficha 1R ha entrado al juego")
-            actuTab()
-        else:
-            print("Eso no parece ser un 5... TURNO DE:  ")
-            dadoB()
-    else: 
-        print("Esta ficha ya está afuera")
-        DadosGen()
-dadoA()
-            
 
 
 
