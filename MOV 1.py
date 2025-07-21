00dce5a658c68a5f733e15b46b3ef60af7eb23dd
Juann2408 <juayalaar@unal.edu.co>
2025-07-21 06:02:51 +0000

    Delete de fichas que acaban 63 casillas

--- MOV 1.py
+++ MOV 1.py
@@ -6,20 +6,23 @@ import random
 Casillas= ["[]"]*68
 Individual1= ["[]"]*7
 Individual2= ["[]"]*7
+Individual3= ["[]"]*7
+Individual4= ["[]"]*7
 fichas = {
     "Jugador ROJO":{
-        "Ficha 1R":{"posicion":0, "estado": "presa en el extranjero"},
-        "Ficha 2R":{"posicion":0, "estado": "presa en el extranjero"},
-        "Ficha 3R":{"posicion":0, "estado": "presa en el extranjero"},
-        "Ficha 4R":{"posicion":0, "estado": "presa en el extranjero"}
+        "Ficha 1R":{"posicion":None, "estado": "presa en el extranjero", "Distancia": 0},
+        "Ficha 2R":{"posicion":None, "estado": "presa en el extranjero", "Distancia": 0},
+        "Ficha 3R":{"posicion":None, "estado": "presa en el extranjero", "Distancia": 0},
+        "Ficha 4R":{"posicion":None, "estado": "presa en el extranjero", "Distancia": 0}
     },
     "Jugador AZUL" :{
-        "Ficha 1AZ":{"posicion":0, "estado": "presa en el extranjero"},
-        "Ficha 2AZ":{"posicion":0, "estado": "presa en el extranjero"},
-        "Ficha 3AZ":{"posicion":0, "estado": "presa en el extranjero"},
-        "Ficha 4AZ":{"posicion":0, "estado": "presa en el extranjero"} 
+        "Ficha 1AZ":{"posicion":None, "estado": "presa en el extranjero", "Distancia": 0},
+        "Ficha 2AZ":{"posicion":None, "estado": "presa en el extranjero", "Distancia": 0},
+        "Ficha 3AZ":{"posicion":None, "estado": "presa en el extranjero", "Distancia": 0},
+        "Ficha 4AZ":{"posicion":None, "estado": "presa en el extranjero", "Distancia": 0}
     }
 }
+
 def extraerFichas (ficha_nombre):
     for playerA, ficha_individual in fichas.items():
             for ficha, datos in ficha_individual.items():
@@ -36,6 +39,8 @@ def actuTab():
     Casillas= ["[]"]*68
     for fichass in fichas.values():
         for ficha_nombre, datos in fichass.items():
+            if datos["Distancia"] > 63 or datos["posicion"] is None:
+                continue  
             poss = datos["posicion"]
             if 0 <= poss <= len(Casillas):
              Casillas[poss]= f"[{ficha_nombre}]"
@@ -46,7 +51,8 @@ def actuTab():
 
 def moverfichas(ficha_nombre, Cant):
     ficha = extraerFichas(ficha_nombre)
-    ficha["posicion"]= (ficha["posicion"] + Cant) % len(Casillas)
+    ficha["posicion"]= (ficha["posicion"] + Cant) % len(Casillas) 
+    ficha["Distancia"] =  (ficha["Distancia"] + Cant)
     actuTab()
 
 def DadosGen(playerA, sumaAmbos):
@@ -61,6 +67,10 @@ def DadosGen(playerA, sumaAmbos):
 
     print(f"Fichas activas disponibles: {', '.join(fichas_activas)}")
     ficha_elegida = input("Escriba el nombre exacto de la ficha que desea mover: ")
+    if ficha_elegida not in fichas_activas:
+        print("Error en digitación. Pierde tuno.")
+        actuTab()
+        return
     moverfichas(ficha_elegida, sumaAmbos)
 
 
@@ -71,7 +81,8 @@ def SacarFicha(playerA, dado):
         for pieza, datos in fichass.items():
             if datos["estado"] != "activa":
                 datos["estado"] = "activa"
-                datos["posicion"] = 5 if playerA == "Jugador ROJO" else 1
+                datos["posicion"] = 5  if playerA == "Jugador ROJO" else 1
+                datos["Distancia"] = 5 if playerA == "Jugador ROJO" else 1
                 print(f"{pieza} ha entrado al juego")
                 actuTab()
                 return
@@ -96,7 +107,7 @@ def INICIO ():
     
     while True:
         TURNOS("Jugador ROJO")
-        if fichas ["Jugador ROJO"]["Ficha 1R"]["posicion"]>= 63:
+        if fichas ["Jugador ROJO"]["Ficha 1R"]["Distancia"]>63 and fichas ["Jugador ROJO"]["Ficha 2R"]["Distancia"]>63 and fichas["Jugador ROJO"]["Ficha 3R"]["Distancia"]>63 and fichas["Jugador ROJO"]["Ficha 4R"]["Distancia"]>63:
             print("Jugador ROJO ha ganado")
 
 
@@ -104,7 +115,7 @@ def INICIO ():
 
         TURNOS("Jugador AZUL")
 
-        if fichas["Jugador AZUL"]["Ficha 1AZ"]["posicion"]>=68:
+        if fichas["Jugador AZUL"]["Ficha 1AZ"]["Distancia"]>68 and fichas["Jugador AZUL"]["Ficha 2AZ"]["Distancia"]>68 and fichas["Jugador AZUL"]["Ficha 3AZ"]["Distancia"]>68 and fichas["Jugador AZUL"]["Ficha 4AZ"]["Distancia"]>68:
             print("Jugador AZUL ha ganado")
             exit()
 #BIENVENIDOS
@@ -115,26 +126,36 @@ def WELCOME ():
     if RULE.upper() == "A":
         print("Perfecto, comencemos a jugar")
         input("Presione ENTER para continuar")
-        INICIO()
+        num_jugadores = int(input("¿Cuántas personas van a jugar? (1/2/3/4) (SE RECOMIENDA 2P por estabilidad): "))
+        if num_jugadores in [1, 2, 3, 4]:
+            if num_jugadores==2:
+                print("ADELANTE")
+                input("Presione ENTER para continuar")
+                INICIO()
+            else:
+                INICIO()
+
     elif RULE.upper() == "B":
         mensaje = """
-        - Cada equipo tiene hasta 4 fichas.
-        - El tablero tiene 68 casillas externas compartidas + 8 casillas de llegada por equipo.
-        - Se usan dos dados estándar (1 al 6).
         - Cada equipo tiene hasta 4 fichas.
         - El tablero tiene 68 casillas externas compartidas + 8 casillas de llegada por equipo.
         - Se usan dos dados estándar (1 al 6).
         - Se lanzan 2 dados por turno.
         - Las fichas solo pueden salir si hay un 5 en el dado.
         - Máximo 2 fichas por casilla
-
-
-
+        - Comer una ficha adiciona al jugador 20 movimientos
+        - Cornonar una ficha otorga 10 movimientos al jugador
+        - Tres pares seguidos conllevan a enviar una ficha a la cárcel
         """
         print(mensaje)
 
         input("Presione ENTER para continuar")
-        INICIO()
+        num_jugadores = int(input("¿Cuántas personas van a jugar? (1/2/3/4) (SE RECOMIENDA 2P por estabilidad): "))
+        if num_jugadores in [1, 2, 3, 4]:
+            if num_jugadores==2:
+                print("ADELANTE")
+                input("Presione ENTER para continuar")
+                INICIO()
     else:
         print("No entendí su respuesta, intentelo de nuevo")
         exit()
