8842e3c784c300b7f1a1e9f814380eed27468dd1
Juann2408 <juayalaar@unal.edu.co>
2025-07-22 06:18:37 +0000

    Cambio

--- Descartado MOVIMIENTO.py
+++ Descartado MOVIMIENTO.py
@@ -1,24 +1,23 @@
-
-#Bienvenida
 import random
+  #Bienvenida
 Dado1 = random.randint(1, 6)
 Dado2=random.randint(1, 6)
-Tablero= []
+Tablero= [0]*21
 
 print(Tablero)
 def movimiento1(Ficha1, Ficha2):
-  while Tablero[Ficha1] != 20 :
-    Dado1 = random.randint(1, 6)
-    Dado2=random.randint(1, 6)
-    print("El resultado es {} y {}".format(Dado1,Dado2))
-    Mueve1= (Dado1 + Dado2)
-    comomoverse=int(input("¿JUGADOR 1. Desea mover únicamente una ficha o dos? Una: 1, Dos: 2 "))
-    if comomoverse==1:
-      Ficha1+=Mueve1
-      print(Ficha1)
-    elif comomoverse==2:
-      CUALFICHA1=int(input("¿JUGADOR 1. ¿Qué ficha desea mover {} movimientos? (1 o 2)".format(Dado1)))
-      CUALFICHA2=int(input("¿JUGADOR 1. ¿Qué ficha desea mover {} movimientos? (1 o 2)".format(Dado2)))
+    while Ficha1 < 20 and Ficha2<20 :
+      Dado1 = random.randint(1, 6)
+      Dado2=random.randint(1, 6)
+      print("El resultado es {} y {}".format(Dado1,Dado2))
+      Mueve1= (Dado1 + Dado2)
+      comomoverse=int(input("¿JUGADOR 1. Desea mover únicamente una ficha o dos? Una: 1, Dos: 2 "))
+      if comomoverse==1:
+        Ficha1+=Mueve1
+        print(Ficha1)
+      elif comomoverse==2:
+        CUALFICHA1=int(input("¿JUGADOR 1. ¿Qué ficha desea mover {} movimientos? (1 o 2)".format(Dado1)))
+        CUALFICHA2=int(input("¿JUGADOR 1. ¿Qué ficha desea mover {} movimientos? (1 o 2)".format(Dado2)))
       if CUALFICHA1==1:
         Ficha1+=Dado1
         Ficha2+=Dado2
@@ -52,4 +51,5 @@ Ficha1=0
 Ficha2=0
 
 Ficha1, Ficha2 = movimiento1(Ficha1, Ficha2), movimiento2(Ficha2)
+
 
